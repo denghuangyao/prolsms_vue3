bc6ee249e45476187edfbe8b571b824bec4027bf
denny <915923471@qq.com>
2025-07-21 17:29:54 +0800

    monorepo架构修改：封装constants包

---
+++ packages/constants/src/core.ts
@@ -0,0 +1,6 @@
+/**
+ * 登录页面 url 地址
+ */
+export const LOGIN_PATH = '/login';
+
+export const TOPMENU_MAXNUM = 10; //菜单栏显示最多个数
---
+++ packages/constants/src/preferences.ts
@@ -0,0 +1,17 @@
+interface systemType {
+  readonly WHP: string;
+  readonly SWYB: string;
+  readonly AQKS: string;
+  readonly GZT: string;
+}
+export const DynamicViews: systemType = {
+  WHP: 'weihuapin',
+  SWYB: 'shengwuYangben',
+  AQKS: 'anquanjiaoyu',
+  GZT: 'Workbenches',
+};
+export const WLConst = {
+  systemType: {
+    ALL: 'ALL',
+  },
+};
