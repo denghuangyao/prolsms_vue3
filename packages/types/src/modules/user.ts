a6d5eee6c9b7343b1f22943ad20e59eb1193b93b
denny <915923471@qq.com>
2025-07-21 17:40:46 +0800

    ts类型遗漏提交

---
+++ packages/types/src/modules/user.ts
@@ -0,0 +1,16 @@
+import type { BasicUserInfo, Recordable } from '..';
+
+interface UserInfo extends BasicUserInfo {
+  //登录首页路径
+  homePath: string;
+  permission?: string[];
+  btnPermission?: string[];
+  menuEntry?: Recordable;
+
+  /**
+   * accessToken
+   */
+  //   token: string
+}
+
+export type { UserInfo };
