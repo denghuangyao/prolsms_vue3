487979fdceab63784f64d6b2134085faa25dadfd
denny <915923471@qq.com>
2025-07-21 17:34:02 +0800

    monorepo架构修改：封装偏好设置相关preferences包

---
+++ packages/preferences/src/config.ts
@@ -0,0 +1,11 @@
+import type { Preferences } from '@dhy/types';
+
+const defaultPreferences: Preferences = {
+  app: {
+    defaultHomePath: '/workbenches',
+  },
+  tabbar: {
+    maxCount: 10,
+  },
+};
+export { defaultPreferences };
